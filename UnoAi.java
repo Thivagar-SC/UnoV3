f7f2cba331feed9d7cf3d6176691c8cf23adbefc
tnnrcantcode <phamhoangbao1501@gmail.com>
2024-06-17 12:33:51 -0400

    Ai get scores

--- UnoAi.java
+++ UnoAi.java
@@ -115,6 +115,15 @@ public class UnoAi {
         }
     }
 
+    public int getAITotalScore() {
+        int totalScore = 0;
+        for (Card card : cards)
+        {
+            totalScore += card.getScoreValue();
+        }
+        return totalScore;
+    }
+
     /**
      * Hits UNO when the AI's hand has one card
      */
